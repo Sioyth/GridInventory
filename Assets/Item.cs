f6fbb5130e3c65333d8f825d660ff148370e5b7e
Sioyth <isioyth@gmail.com>
2022-06-29 18:33:27 +0200

    Items can now be moved around

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -14,20 +14,25 @@ public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
     [SerializeField] private Image _icon;
     [SerializeField] private Image _backGround;
 
+
+    private Vector2 _lastMousePosition;
     private void Awake()
     {
-        RectTransform transform = GetComponent<RectTransform>();
-        transform.sizeDelta = _slotSize * Inventory.CellSize;
+        _icon.transform.GetComponent<RectTransform>().sizeDelta = _slotSize * Inventory.CellSize;
+        _backGround.transform.GetComponent<RectTransform>().sizeDelta = _slotSize * Inventory.CellSize;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-
+        _lastMousePosition = eventData.position;
     }
 
     public void OnDrag(PointerEventData data)
     {
-       
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        Vector2 delta =data.position - _lastMousePosition;
+        rectTransform.position = rectTransform.position + new Vector3(delta.x, delta.y, rectTransform.position.z);
+        _lastMousePosition = data.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
